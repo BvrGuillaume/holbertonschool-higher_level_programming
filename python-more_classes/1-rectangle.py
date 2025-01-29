a5d2044fbe68f8c03420afac716b8b01dc482a06
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-01-29 15:16:29 +0100

    add private instance attributes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,22 +5,22 @@ This module defines an empty class called Rectangle.
 
 
 class Rectangle:
-    """An empty class that defines a rectangle."""
+    """ an empty rectangle, we will define it later too
+    """
 
-
-def __init__(self, width=0, height=0):
-    """Initialize the rectangle with width and height"""
-    self.width = width
-    self.height = height
+    def __init__(self, width=0, height=0):
+        """initialize the rectangle with width and height at 0"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Retrieve the width."""
+        """get width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-
+        """change the size of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,12 +29,12 @@ def __init__(self, width=0, height=0):
 
     @property
     def height(self):
-        """Set the height with validation"""
+        """get the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-
+        """ change the height of the rectangle with the value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
