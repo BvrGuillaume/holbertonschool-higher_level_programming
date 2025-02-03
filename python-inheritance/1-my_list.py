52ea4b1b6cf5baa8bba1350294faa0b6a7782009
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-03 15:30:34 +0100

    class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 class MyList(list):
-    """A subclass of list that adds a method to print the list in sorted order."""
-    
+    """A subclass of list that adds a method to print the list
+      in sorted order."""
+
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original list."""
+        """Prints the list in ascending order without modifying the
+          original list."""
         try:
 
             print(sorted(self))
