2ba32d18037c20b29ebd6e14178daa297d73a2f3
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-03 13:29:08 +0100

    function that return the list

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to create class
+This module defines an empty class called list.
+"""
+
+
+def lookup(obj):
+    """Empty class that defines a square.
+
+    This class doesn't currently have any properties, methods, or attributes.
+    It is essentially a placeholder for future development.
+    """
+
+    return dir(obj)
