7a617e6fb240d7db25d5e6a01fe4ca3a9a486556
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-04 12:50:35 +0100

    create a empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to create class
+"""
+
+
+class BaseGeometry:
+
+    """A base class for geometry.
+
+        This class serves as a blueprint for future geometry-related classes.
+        Currently, it does not implement any functionality.
+    """
+    pass
