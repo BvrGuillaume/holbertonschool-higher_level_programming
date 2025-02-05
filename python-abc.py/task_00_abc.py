7e4545eff5fc452501f7c1012bd842e3dd03bb28
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-05 14:24:37 +0100

    create dog&cat sound

---
+++ python-abc.py/task_00_abc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module to create class"""
+
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    """Animal class"""
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    """A class representing a soubclass of animal"""
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """A class representing a soubclass of animal"""
+    def sound(self):
+        return "Meow"
