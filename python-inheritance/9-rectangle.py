a6d27ab3dc0b38a8981cb946bb63b772dc4b6275
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-05 10:20:26 +0100

    area function

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -22,10 +22,10 @@ class BaseGeometry:
             TypeError: If value is not an integer
             ValueError: If value to be validated
         """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
