5be1739647bb954b7792649b4273fb9026136034
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-05 10:24:27 +0100

    area function

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -12,16 +12,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validates if a value is a positive integer
-
-        Args:
-            name (str): The name of the value.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If value is not an integer
-            ValueError: If value to be validated
-        """
+        """ Validates if a value is a positive integer"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
