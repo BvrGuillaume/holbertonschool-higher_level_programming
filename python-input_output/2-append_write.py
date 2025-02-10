49cc6a268527a6ab872ef4d3cd207dd88e17ca4c
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-10 13:59:01 +0100

    append write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,5 +3,14 @@
 
 
 def append_write(filename="", text=""):
+    """Appends a string to a text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The text to append to the file.
+
+    Returns:
+        int: The number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
