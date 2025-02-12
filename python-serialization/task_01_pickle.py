6172d61c00e7726bfb2d9c0b6d3cab3aecd62ff6
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-12 10:59:43 +0100

    serialize and deserialize custom Python objects using the pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def display(self):
+    print(f"Name: {self.name}")
+    print(f"Age: {self.age}")
+    print(f"Is Student: {self.is_student}")
+
+
+def serialize(self, filename: str):
+    try:
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+    except Exception as e:
+        print(f"Serialization error: {e}")
+
+
+@classmethod
+def deserialize(cls, filename: str):
+    try:
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
+    except (FileNotFoundError, pickle.PickleError, EOFError) as e:
+        print(f"Deserialization error: {e}")
+        return None
