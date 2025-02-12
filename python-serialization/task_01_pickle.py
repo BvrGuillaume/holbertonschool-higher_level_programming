df0bdca41618650882a721b5775c36a9fab0ad59
BvrGuillaume <bouvier.guillaume53@gmail.com>
2025-02-12 11:20:48 +0100

    serialize and deserialize custom Python objects using the pickle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -5,32 +5,29 @@ import pickle
 class CustomObject:
     """Defines a custom object with name, age, and student status."""
 
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
-def __init__(self, name, age, is_student):
-    self.name = name
-    self.age = age
-    self.is_student = is_student
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-
-def display(self):
-    print(f"Name: {self.name}")
-    print(f"Age: {self.age}")
-    print(f"Is Student: {self.is_student}")
-
-
-def serialize(self, filename: str):
-    try:
-        with open(filename, 'wb') as file:
-            pickle.dump(self, file)
-    except Exception as e:
-        print(f"Serialization error: {e}")
+    def serialize(self, filename: str):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Serialization error: {e}")
 
 
-@classmethod
-def deserialize(cls, filename: str):
-    try:
-        with open(filename, 'rb') as file:
-            return pickle.load(file)
-    except (FileNotFoundError, pickle.PickleError, EOFError) as e:
-        print(f"Deserialization error: {e}")
-        return None
+    @classmethod
+    def deserialize(cls, filename: str):
+        try:
+            with open(filename, 'rb') as file:
+             return pickle.load(file)
+        except (FileNotFoundError, pickle.PickleError, EOFError) as e:
+            print(f"Deserialization error: {e}")
+            return None
